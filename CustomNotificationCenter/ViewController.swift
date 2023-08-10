3de6111ab824e8830ae8ff47c00e17c46aef6076
Akash Rastogi <akash_rastogi@live.com>
2023-08-11 00:46:03 +0530

    test notifictaion

--- CustomNotificationCenter/ViewController.swift
+++ CustomNotificationCenter/ViewController.swift
@@ -8,12 +8,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    CustomNotificationCenter.shared.postNotification("viewDidLoadNotification", object: ["viewDidLoad": true])
   }
-
-
 }
-
